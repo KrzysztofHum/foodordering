c76bc3ccfc05cddcbd97de1753e49c3f7683d590
KrzysztofHum <k.humienny1996@protonmail.com>
2024-05-20 10:33:37 +0200

    product details screen

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,12 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams } from "expo-router";
 
 const ProductDetailsScreen = () => {
   const { id } = useLocalSearchParams();
   return (
     <View>
-      <Text >ProductDetailsScreen for id: {id}</Text>
+      <Stack.Screen options={{ title: "Details" + id }} />
+      <Text>ProductDetailsScreen for id: {id}</Text>
     </View>
   );
 };
